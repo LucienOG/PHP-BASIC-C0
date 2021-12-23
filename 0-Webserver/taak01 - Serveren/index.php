52f5856859d0b1e3b44b7cbdc490a0d4899e7b3d
Lucien Osei <lucien.osei@student.rocva.nl>
2021-12-23 14:52:22 +0100

    data en tekst

--- 0-Webserver/taak01 - Serveren/index.php
+++ 0-Webserver/taak01 - Serveren/index.php
@@ -3,7 +3,10 @@
     $naam = "Lucien";
     $leeftijd = "17";
     $melk = 1.19;
-    
+    $test = true;
+    $test = null;
+    $voornaam = "Tom";
+
     str_repeat('&nbsp;', 1); 
 
     echo "$score &nbsp";
@@ -12,9 +15,31 @@
 
     echo "$leeftijd &nbsp";
 
-    echo "$melk";
+    echo "$melk &nbsp";
 
+    echo "$test &nbsp";
+    
+    echo gettype($voornaam);
 
+    $string = "ROCVA";
+    $integer = 2;
+    $float  =  9.9;
+    $boolean = true;
+    $null = null;
 
+    echo gettype($string);
+    echo gettype($integer);
+    echo gettype($float);
+    echo gettype($boolean);
+    echo gettype($null);
+
+    $Voornaam = "Lucien";
+    $Leeftijd = 17;
+
+    echo "Jouw naam is $Voornaam en je bent je $Leeftijd jaar oud.";
+
+    $voornaam1 = "lucien";
+    $voornaam2 = "gabriel";
+    $voornaam3 = "gabe";
 
 
---
+++ 2-Variabelen-en-datatype/2.7 - data en tekst combineren/index.php
@@ -0,0 +1,6 @@
+<?php
+
+    $Voornaam = "Lucien";
+    $Leeftijd = 17;
+
+    echo "Jouw naam is $Voornaam en je bent je $Leeftijd jaar oud.";
