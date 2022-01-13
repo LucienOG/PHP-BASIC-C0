2bd8400d12acf6d940ba1494ff24a38ea5b6e8eb
Lucien Osei <lucien.osei@student.rocva.nl>
2022-01-13 13:08:32 +0100

    changed order

---
+++ 3-Arrays/3.5 - Array sorteren/index.php
@@ -0,0 +1,11 @@
+<?php
+    $getallen = [676, 5345, 78657, 3, 46, 21, 47];
+    asort($getallen);
+    $laag_hoog = $getallen;
+    print_r($laag_hoog);
+
+    arsort($getallen);
+    $hoog_laag = $getallen;
+    print_r($hoog_laag);    
+
+?>
