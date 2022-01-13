e7f0f908c2813b55ba7ffc0bb32477dba9b3bcd8
Lucien Osei <lucien.osei@student.rocva.nl>
2022-01-13 11:34:14 +0100

    concetation 2

--- 2-Variabelen-en-datatype/2.10 - String concatenation 2/index.php
+++ 2-Variabelen-en-datatype/2.10 - String concatenation 2/index.php
@@ -1,5 +1,11 @@
 <?php
 
-    $waarde = "<li> een waarde </li>";
+    $string1 = "een waarde 1";
+    $string2 .=" een waarde 2";
+    $string3 .=" een waarde 3";
+
+    echo "<ul> <li>$string1</li> <li>$string2</li> <li>$string3</li> </ul>"
+        
+
 
 ?>
