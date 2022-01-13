5ddd56f47f86f608a3f66ae4ab166fc68671418e
Lucien Osei <lucien.osei@student.rocva.nl>
2022-01-13 12:53:04 +0100

    multidimensional array

---
+++ 3-Arrays/3.4 - Multidimensionaal/index.php
@@ -0,0 +1,8 @@
+<?php
+    $abcGteallen =  [ ['a', 'b', 'c', 'd'], [24, 56, 78, 90] ];
+
+    print_r($abcGteallen);
+    echo $abcGteallen[0][0].$abcGteallen[1][1];
+    echo $abcGteallen[0][2].$abcGteallen[1][3];
+
+?>
