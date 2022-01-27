616b99900536afc893bac7ca39dc6bd271526795
Lucien Osei <lucien.osei@student.rocva.nl>
2022-01-27 11:03:31 +0100

    if else conditional

---
+++ 5-Conditional_Statements/5.3 - if, else conditional/index.php
@@ -0,0 +1,54 @@
+<?php
+    $test1 = 3;
+    $test2 = 6;
+
+    if($test1 == $test2){
+        echo "gelijk" . "<br>";
+    }
+    else {
+        echo "niet gelijk" . "<br>";
+    }
+
+    if($test1 != $test2){
+        echo "ongelijk" . "<br>";
+    }
+    else {
+        echo "gelijk" . "<br>";
+    }
+
+    if($test1 === $test2){
+        echo "identiek" . "<br>";
+    }
+    else {
+        echo "niet identiek" . "<br>";
+    }
+
+    if($test1 > $test2){
+        echo "test1 is groter dan test2" . "<br>";
+    }
+    else {
+        echo "test1 is kleiner dan test2" . "<br>";
+    }
+
+    if($test1 < $test2){
+        echo "test1 is kleiner dan test2" . "<br>";
+    }
+    else {
+        echo "test1 is groter dan test2" . "<br>";
+    }
+
+    if($test1 >= $test2){
+        echo "test1 is groter of gelijk aan test2" . "<br>";
+    }
+    else {
+        echo "test1 is kleiner of gelijk aan test2" . "<br>";
+    }
+
+    if($test1 <= $test2){
+        echo "test1 is kleiner of gelijk aan test2" . "<br>";
+    }
+    else {
+        echo "test1 is groter of gelijk aan test2" . "<br>";
+    }
+ 
+?>
