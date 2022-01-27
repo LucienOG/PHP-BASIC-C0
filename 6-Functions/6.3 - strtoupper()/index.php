2277223aa83b973ef3737fe291efabf592a2119c
Lucien Osei <lucien.osei@student.rocva.nl>
2022-01-27 14:47:04 +0100

    strtoupper

---
+++ 6-Functions/6.3 - strtoupper()/index.php
@@ -0,0 +1,10 @@
+<?php
+    function telAlleKarakters() {
+        $var1 = "Mijn blauwe auto is een mooie snelle wagen";
+        $aantal = strlen($var1);
+        $lengteRegel = $aantal;
+        echo $lengteRegel;
+    }
+
+    telAlleKarakters()
+?>
