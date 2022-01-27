18d5383eacb813c9dc09e0d8ff44a2e2037b5e0d
Lucien Osei <lucien.osei@student.rocva.nl>
2022-01-27 11:47:03 +0100

    switch conditional

---
+++ 5-Conditional_Statements/5.5 - switch conditional/index.php
@@ -0,0 +1,88 @@
+<?php
+    $score = 15;
+
+    switch($score) {
+        case 15:
+        echo "Wow wat een fantastische score!" . "<br>";
+        break;
+        case 14:
+        echo "Wow bijna alle punten nog 1 te gaan!" . "<br>";
+        break;
+        case 13:
+        echo "Goed bezig, bijna alle punten!" . "<br>";
+        break;
+        case 12:
+        echo "Ga zo door!" . "<br>";
+        break;
+        default:
+        echo "Nog even oefenen dan haal je vast alle punten!" . "<br>";
+    }
+
+    switch($score) {
+        case 16:
+        echo "Wow wat een fantastische score!" . "<br>";
+        break;
+        case 15:
+        echo "Wow bijna alle punten nog 1 te gaan!" . "<br>";
+        break;
+        case 14:
+        echo "Goed bezig, bijna alle punten!" . "<br>";
+        break;
+        case 13:
+        echo "Ga zo door!" . "<br>";
+        break;
+        default:
+        echo "Nog even oefenen dan haal je vast alle punten!" . "<br>";
+    }
+
+    switch($score) {
+        case 17:
+        echo "Wow wat een fantastische score!" . "<br>";
+        break;
+        case 16:
+        echo "Wow bijna alle punten nog 1 te gaan!" . "<br>";
+        break;
+        case 15:
+        echo "Goed bezig, bijna alle punten!" . "<br>";
+        break;
+        case 14:
+        echo "Ga zo door!" . "<br>";
+        break;
+        default:
+        echo "Nog even oefenen dan haal je vast alle punten!" . "<br>";
+    }
+
+    switch($score) {
+        case 18:
+        echo "Wow wat een fantastische score!" . "<br>";
+        break;
+        case 17:
+        echo "Wow bijna alle punten nog 1 te gaan!" . "<br>";
+        break;
+        case 16:
+        echo "Goed bezig, bijna alle punten!" . "<br>";
+        break;
+        case 15:
+        echo "Ga zo door!" . "<br>";
+        break;
+        default:
+        echo "Nog even oefenen dan haal je vast alle punten!" . "<br>";
+    }
+
+    switch($score) {
+        case 19:
+        echo "Wow wat een fantastische score!" . "<br>";
+        break;
+        case 18:
+        echo "Wow bijna alle punten nog 1 te gaan!" . "<br>";
+        break;
+        case 17:
+        echo "Goed bezig, bijna alle punten!" . "<br>";
+        break;
+        case 16:
+        echo "Ga zo door!" . "<br>";
+        break;
+        default:
+        echo "Nog even oefenen dan haal je vast alle punten!" . "<br>";
+    }
+?>
