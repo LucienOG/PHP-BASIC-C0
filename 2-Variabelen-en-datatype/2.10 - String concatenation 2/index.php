56326c8d6f60edbddee54a829479cd1aced462bd
Lucien Osei <lucien.osei@student.rocva.nl>
2022-02-18 11:37:01 +0100

    verbeterd

--- 2-Variabelen-en-datatype/2.10 - String concatenation 2/index.php
+++ 2-Variabelen-en-datatype/2.10 - String concatenation 2/index.php
@@ -1,11 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "<ul>";
 
-    $string1 = "een waarde 1";
-    $string2 .=" een waarde 2";
-    $string3 .=" een waarde 3";
+    $item = "<li>een waarde 1</li>";
+    $item .= "<li>een waarde 2</li>";
+    $item .= "<li>een waarde 3</li>";
+    echo $item;
+    echo "</ul>";
 
-    echo "<ul> <li>$string1</li> <li>$string2</li> <li>$string3</li> </ul>";
+
+
         
-
-
-?>
+    ?>
+</body>
+</html>
